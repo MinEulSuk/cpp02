eee4cdcd6fa743c52aa641ee51fde322eb9c7ec9
MinEulSuk <202130208@daelim.ac.kr>
2025-09-04 10:51:29 +0900

    README.md .gitignore

---
+++ week02/app01.cpp
@@ -0,0 +1,12 @@
+#include "invoice.h"
+
+int main()
+{
+	Product product1("½ÄÅ¹", 210.00);
+	Product product2("ÀÇÀÚ", 680.00);
+	Invoice invoice(1000);
+	invoice.add(1, product1);
+	invoice.add(4, product2); 
+	invoice.print();
+	return 0;
+}
