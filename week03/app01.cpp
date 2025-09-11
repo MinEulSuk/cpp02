eb580daa8bf27c45c73271e9af59204408c519ec
MinEulSuk <202130208@daelim.ac.kr>
2025-09-11 11:28:19 +0900

    week03 ver0.2 virtual

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -15,17 +15,15 @@ public:
 class Pikachu : public Pokemon // 상속 is-a 관계
 {
 public:
-	void print() const { cout << "전기 공격" << endl; }
+	void attack() const override { cout << "전기 공격" << endl; }
 };
 int main()
 {
-	// 베이스 클래스에 대한 포인터(소켓) 생성
 	Pokemon* pokemon;
-	// ptr 포인터로 베이스 클래스의 객체 가리키기
 	pokemon = new Pokemon();
 	pokemon->attack();
 	delete pokemon;
-	// ptr 포인터로 파생 클래스의 객체 가리키기
+
 	pokemon = new Pikachu();
 	pokemon->attack();
 	delete pokemon;
