1c4d87496cd00fe888baa556f3432044df13fc04
MinEulSuk <202130208@daelim.ac.kr>
2025-09-25 11:52:05 +0900

    week05 v1.1 pure virtual function

--- week05/app.cpp
+++ week05/app.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 class Animal {
 public:
-	virtual void makeSound() {
+	virtual void makeSound() = 0 { //순수 가상 함수
 		cout << "동물이 소리를 냅니다.\n";
 	};
 };
@@ -26,34 +26,11 @@ public:
 };
 int main()
 {
-	Animal* pa = new Dog(); //자식클래스의 객체를 부모클래스 포인터로 참조 , 업캐스팅
-	pa->makeSound(); // 가상함수가 아니므로 부모클래스의 멤버함수 호출
-	cout << pa << endl;
-
-	//Cat* pc = dynamic_cast<Cat*>(pa); //safe 고양이클래스의 포인터가 강아지클래스의 메모리 주소를 참조하려고 하므로 다운캐스팅이 실패
-	Dog* pc = dynamic_cast<Dog*>(pa);
-	cout << pc << endl; // 상속관계에서 다운캐스팅이 실패하면 nullptr 반환
-	pc->makeSound();
-	cout << pc << endl;
-	delete pc;
-	pc = nullptr;
-
-
-	//Cat* pc = (Cat*)pa;//위험한 행동
-	//pc->makeSound(); // 고양이가 출력되어야 하는데 강제 형변환으로 인해 잘못된 결과 출력 
-	//cout << pc << endl;
-	//delete pc;
-	//pc = nullptr;
+	Animal animal;//순수	 가상 함수는 객체 생성 불가
+	animal.makeSound();
 
 
 
-	//Dog* pd = (Dog*)pa; //부모클래스 포인터를 자식클래스 포인터로 형변환 , 다운캐스팅 (old,C스타일)
-	//Dog* pd = dynamic_cast<Dog*>(pa); //다운캐스팅 (modern, C++스타일)
-	//pd->makeSound(); // 자식클래스의 멤버함수 호출
-	//delete pd;
-	//pd = nullptr;
 
 	return 0;
-
-	//git reset --hard HEAD~삭제할커밋개수
 }
