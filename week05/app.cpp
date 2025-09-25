f4d54eddf7444810ea097edc3d6043b4e4011e91
MinEulSuk <202130208@daelim.ac.kr>
2025-09-25 11:32:54 +0900

    week05 v0.9 unsave casting

--- week05/app.cpp
+++ week05/app.cpp
@@ -28,9 +28,10 @@ int main()
 {
 	Animal* pa = new Dog(); //자식클래스의 객체를 부모클래스 포인터로 참조 , 업캐스팅
 	pa->makeSound(); // 가상함수가 아니므로 부모클래스의 멤버함수 호출
-
+	cout << pa << endl;
 	Cat* pc = (Cat*)pa;//위험한 행동
 	pc->makeSound(); // 고양이가 출력되어야 하는데 강제 형변환으로 인해 잘못된 결과 출력 
+	cout << pc << endl;
 	delete pc;
 	pc = nullptr;
 
@@ -43,4 +44,6 @@ int main()
 	//pd = nullptr;
 
 	return 0;
+
+	//git reset --hard HEAD~삭제할커밋개수
 }
