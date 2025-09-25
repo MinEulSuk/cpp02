13f8f342220fb1e85cef395e85426c31f819cd0d
MinEulSuk <202130208@daelim.ac.kr>
2025-09-25 17:57:20 +0900

    week07 v0.2 deadly diamond problem

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
+class Person {
+public :
+	string name;
+	virtual void warn() {};
+};
+
 class UndergraduateStudent {
 public : 
+	double gpa;
 	void warn() { cout << "학사경고\n"; };
 };
 class DormitoryStudent{
 public :
+	int roomNumber;
 	void warn() { cout << "벌점부여\n"; };
 };
 class UndergraduateDormitoryStudent
@@ -18,5 +26,8 @@ int main() {
 	UndergraduateDormitoryStudent uds;
 	//uds.warn(); // 다중상속 시 어느 부모의 warn함수를 호출할 지 모호함
 	uds.DormitoryStudent::warn();
+	uds.gpa = 3.9;
+	uds.roomNumber = 201;
+	//uds.name = "민의석"; // 에러 발생 죽음의 다이아몬드 문제
 	return 0;
 }
