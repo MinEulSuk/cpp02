09e335cd9e4c353b25001c4ad71a5e14a13d2a6e
MinEulSuk <202130208@daelim.ac.kr>
2025-09-25 18:23:14 +0900

    week07 v0.3 virtual inheritance

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -5,15 +5,15 @@ using namespace std;
 class Person {
 public :
 	string name;
-	virtual void warn() {};
+	//virtual void warn() {};
 };
 
-class UndergraduateStudent {
+class UndergraduateStudent :virtual public Person{
 public : 
 	double gpa;
 	void warn() { cout << "학사경고\n"; };
 };
-class DormitoryStudent{
+class DormitoryStudent : virtual public Person {
 public :
 	int roomNumber;
 	void warn() { cout << "벌점부여\n"; };
@@ -28,6 +28,10 @@ int main() {
 	uds.DormitoryStudent::warn();
 	uds.gpa = 3.9;
 	uds.roomNumber = 201;
-	//uds.name = "민의석"; // 에러 발생 죽음의 다이아몬드 문제
+	//부모 객체는 상관없으나 양쪽에서 상속을 받으면 다이아몬드 문제로 에러
+	uds.name = "민의석"; // 에러 발생 죽음의 다이아몬드 문제
+	//양쪽 부모 클래스가 할아버지 클래스를 상속받을 때 virtual 키워드를 이용해 해결
+
+	cout << uds.name << "의 평점은" << uds.gpa << "이고 , 기숙사 방번호는 " << uds.roomNumber << "입니다.";
 	return 0;
 }
