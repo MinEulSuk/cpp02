301801d6f4944e7e53d9ebc8c3472f651b3788f2
MinEulSuk <202130208@daelim.ac.kr>
2025-11-20 12:27:17 +0900

    week13 v3.7 function template

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -3,26 +3,24 @@ using namespace std;
 
 // 템플릿 함수 정의
 template <typename T>
-void exchange(T* first, T* second)
+T bigger(T first, T second)
 {
-	T temp = *first;
-	*first = *second;
-	*second = temp;
+	if (first > second)
+		return first;
+	else
+		return second;
 }
 
 int main()
 {
-	// int 자료형 스왑
-	int integer1 = 5;
-	int integer2 = 70;
-	swap(integer1, integer2);
-	cout << "swap(5, 70) = ";
-	cout << integer1 << "  " << integer2 << endl;
-	// double 자료형 스왑
-	double d1 = 101.5;
-	double d2 = 402.7;
-	swap(d1, d2);
-	cout << "swap(101.5, 402.7) = ";
-	cout << d1 << "  " << d2 << endl;
+	int i1 = 11;
+	double d1 = 76.97;
+	cout << bigger<double>(i1, d1) << endl;
+	cout << bigger(55.7, 11.9) << endl;
+	cout << bigger("hello", "hi") << endl; //메모리 주소 기준으로 출력
+	cout << bigger<string>("hello", "hi") << endl; // he hi까지 비교해서 i가 더 크니 hi 출력
+	cout << bigger<string>("hxllo", "hi") << endl; // hx hi까지 비교해서 x가 더 크니 hxllo 출력
+	cout << bigger<string>("Hxllo", "hi") << endl; // H가 h 보다 작으니 hi 출력
+	cout << bigger<string>("hxllo", "hxlloooo") << endl; //
 	return 0;
 }
---
+++ week13/prac.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//using namespace std;
+//
+//// 템플릿 함수 정의
+////template <typename T>
+////void exchange(T first, T second)
+////{
+////	T temp = first;
+////	first = second;
+////	second = temp;
+////}
+//
+//auto sort = [](auto& first, auto& second) {
+//	auto temp = first;
+//	first = second;
+//	second = temp;
+//	};
+//template <typename T>
+//void swap(T& first, T& second) {
+//	T temp = first;
+//	first = second;
+//	second = temp;
+//}
+//int main()
+//{
+//
+//	// int 자료형 스왑
+//	int i1 = 5;
+//	int i2 = 70;
+//	sort(i1, i2);
+//	cout << i1 << "  " << i2 << endl;
+//	// double 자료형 스왑
+//	//double d1 = 101.5;
+//	//double d2 = 402.7;
+//	//swap(d1, d2);
+//	//cout << d1 << "  " << d2 << endl;
+//	return 0;
+//}
