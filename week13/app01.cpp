b60870ddd9757d262c2815fd40c2670168cd252c
MinEulSuk <202130208@daelim.ac.kr>
2025-11-20 12:27:33 +0900

    week13 v3.8 function template

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 using namespace std;
 
-// 템플릿 함수 정의
 template <typename T>
-T bigger(T first, T second)
-{
-	if (first > second)
-		return first;
-	else
-		return second;
+void display(T value) {
+	cout << "Generic display : " << value << '\n';
 }
 
+template <>
+void display<char>(char value) {
+	cout << "Specialized display for char : " << value << endl;
+}
+
+template <> // 문자열 리터럴
+void display<const char*>(const char* value) {
+	cout << "Specialized display for c style string : " << value << endl;
+}
+
+
+
 int main()
-{
-	int i1 = 11;
-	double d1 = 76.97;
-	cout << bigger<double>(i1, d1) << endl;
-	cout << bigger(55.7, 11.9) << endl;
-	cout << bigger("hello", "hi") << endl; //메모리 주소 기준으로 출력
-	cout << bigger<string>("hello", "hi") << endl; // he hi까지 비교해서 i가 더 크니 hi 출력
-	cout << bigger<string>("hxllo", "hi") << endl; // hx hi까지 비교해서 x가 더 크니 hxllo 출력
-	cout << bigger<string>("Hxllo", "hi") << endl; // H가 h 보다 작으니 hi 출력
-	cout << bigger<string>("hxllo", "hxlloooo") << endl; //
+{//generic display로 받지만 char이 있으면 char로 받음
+
+	display(50);
+	display('2'); //
+	display("2"); //
+	display(2.17);
+
 	return 0;
 }
